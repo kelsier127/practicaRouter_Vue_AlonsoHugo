98b3c8a046af73060935e2eaf7395f53baefeb25
Hugo <hugoalonso343@gmail.com>
2024-12-03 20:00:52 +0100

    pep

--- src/views/ExperienceView.vue
+++ src/views/ExperienceView.vue
@@ -1,11 +1,15 @@
 <script setup>
 import data from '@/assets/data.json';
+import { ref } from 'vue'
+import { useRoute } from 'vue-router';
+
+
 
 </script>
 
 <template>
   <div>
-    
+    {{ destino }}
   </div>
 </template>
 
